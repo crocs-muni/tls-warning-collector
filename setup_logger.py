e89b80b6110991fae5a03db5fff744b36094bc93
Gontko Filip <filip.gontko@avast.com>
2019-08-14 11:34:26 +0200

    changed logger path to logs directory

--- setup_logger.py
+++ setup_logger.py
@@ -3,7 +3,7 @@ import time
 
 # Set-up basic configuration. This needs to be set-up only once.
 timestamp = time.strftime("%d-%m-%Y")
-filename = 'logfile' + timestamp + '.log'
+filename = '/logs/logfile' + timestamp + '.log'
 logging.basicConfig(filename='', format='%(asctime)s - %(levelname)s - %(message)s',
                     datefmt='%d-%b-%y %H:%M:%S', level=0)
 # Creating logger which will be used in other files.
