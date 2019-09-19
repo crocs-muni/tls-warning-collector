a605611825ba308f9f82cb8245fde2e477220505
filipgontko <filip.gontko@gmail.com>
2019-09-19 10:54:14 +0200

    Added check if using Admin CMD and check it in main
    Fix #49

--- requirements.py
+++ requirements.py
@@ -6,23 +6,34 @@ CURRENT_DIR = os.getcwd()
 
 
 def check_requirements():
-    print("Check drivers.")
-    drivers_path = CURRENT_DIR + "\\drivers"
-    cmd = 'echo %Path% | find "tls-warning-collector-dev\\drivers"'
+	"""Checks if all requirements are met."""
+    check_if_admin()
+ 	check_if_in_path()
+    pip_cmd = 'pip3 install --quiet -r requirements.txt'
+    subprocess.call(pip_cmd, shell=True)
+    time.sleep(1)
+    return
+
+
+def check_if_in_path():
+	"""Checks if drivers folder is in PATH"""
+	drivers_path = CURRENT_DIR + "\\drivers"
+    cmd = 'echo %Path% | find "tls-warning-collector-dev\\drivers" >nul'
     proc = subprocess.call(cmd, shell=True)
     if proc == 0:
-        # Everything went well and the drivers are in Path
-        print("Drivers are in path. Continue installation...")
+        pass
     else:
         print("Drivers are NOT in Path. Follow instructions in the drivers folder and then re-run this program.")
-        return 0
-    time.sleep(1)
-    print("Installing requirements.")
-    pip_cmd = 'pip3 install -r requirements.txt'
-    subprocess.Popen(pip_cmd)
-    time.sleep(1)
-    return 0
+        return 
 
 
-if __name__ == '__main__':
-    check_requirements()
+def check_if_admin():
+	"""Checks if user has Administrator Command Prompt running"""
+	cmd = 'net session >nul 2>&1'
+	is_admin = subprocess.call(cmd, shell=True)
+	if is_admin == 0:
+		pass
+	else:
+		print("You are not and Administrator. Change to Administrator Command Line in order to run this.")
+		return
+	return
