8ac2ee06ae336dcc89495a6b066e82207c7a4e30
filipgontko <filip.gontko@gmail.com>
2020-05-16 13:41:18 +0200

    Fixed: #62 browser installation failure

--- main.py
+++ main.py
@@ -32,49 +32,53 @@ cfg = read_config()
 def main():
     """Iterates over all of the browsers and versions and runs the script for getting screenshots."""
     check_requirements()
-    all_browsers = len(read_config()['browserIDs'])
-    iteration = 0
-    for browserID in read_config()['browserIDs']:
-        iteration += 1
-        all_versions = cfg['browsers'][browserID]['one-version']
-        v_iteration = 0
-        progress = set_progress_percentage(iteration, all_browsers)
-        for version in all_versions:
-            v_iteration += 1
-            v_progress = set_progress_percentage(v_iteration, len(all_versions))
+    all_browsers = len(read_config().get('browserIDs'))
+    for index, browserID in enumerate(read_config().get('browserIDs')):
+        all_versions = cfg.get('browsers')[browserID].get('one-version')
+        progress = set_progress_percentage(index + 1, all_browsers)
+        for v_index, version in enumerate(all_versions):
+            v_progress = set_progress_percentage(v_index + 1, len(all_versions))
             logger.info('######## Processing %s v(%s)', browserID, version)
             if browserID != 'edge':
-                install_browser(browserID, version)
+                return_code = install_browser(browserID, version)
+                if return_code != 0:
+                    logger.error("# Installation failed...")
+                    uninstall_browser(browserID)
+                    logger.info("# Skipping to the next browser version.")
+                    continue
             get_ssl_screenshot(browserID, version)
             print_progress(v_progress, versions=True)
             uninstall_browser(browserID)
         print_progress(progress)
-    return
-
 
 
 def install_browser(browser, version):
     """Installs the given browsers version."""
-    cmd = "choco install " + str(cfg['browsers'][browser]['package']) + " --force --version=" + str(version) + \
+    cmd = "choco install " + str(cfg.get('browsers')[browser].get('package')) + " --force --version=" + str(version) + \
           " --yes --nocolor --limit-output --no-progress --ignore-checksums --log-file=choco-log.log"
-    logger.info("# Installing the browsers.")
-    subprocess.Popen(cmd)
-    time.sleep(60)
-    logger.info("# Installation done.")
+    logger.info("# Installing the browser.")
+    process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    logger.info("# Please wait...")
+    for _ in process.stdout.readlines():
+        process.wait()
+    rc = process.returncode
+    return rc
 
 
 def uninstall_browser(browser):
     """Uninstalls the given browsers."""
-    cmd = "choco uninstall " + str(cfg['browsers'][browser]['package']) + \
+    cmd = "choco uninstall " + str(cfg.get('browsers')[browser].get('package')) + \
           " --allversions --yes --nocolor --limit-output --log-file=choco-log.log"
     logger.info("# Uninstalling the browsers.")
     subprocess.Popen(cmd)
-    time.sleep(60)
+    process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    logger.info("# Please wait...")
+    for _ in process.stdout.readlines():
+        process.wait()
     logger.info("# Uninstalling done.")
     logger.info("# Removing installation folders.")
-    for folder in cfg['browsers'][browser]['installFolders']:
+    for folder in cfg.get('browsers')[browser].get('installFolders'):
         remove_item(folder)
-    logger.info("All folders are removed.")
 
 
 def remove_item(item):
@@ -94,15 +98,15 @@ def get_ssl_screenshot(browser, version):
     """Gets the screenshot of SSL warning in the given browsers version."""
     # Loop through all cases
     logger.info("# Preparing iteration.")
-    all_cases = len(cfg['cases'])
+    all_cases = len(cfg.get('cases'))
     iteration = 0
     for case in cfg['cases']:
         iteration += 1
         progress = set_progress_percentage(iteration, all_cases)
         try:
             output(browser, str(version), case)
-            open_webpage(cfg['browsers'][browser]['binary'], cfg['cases'][case]['url'], case, str(version),
-                     cfg['browsers'][browser]['package'])
+            open_webpage(cfg.get('browsers')[browser].get('binary'), cfg.get('cases')[case].get('url'), case, str(version),
+                     cfg.get('browsers')[browser].get('package'))
         except Exception as e:
             logger.error("Something went TERRIBLY wrong. - %s", e)
         print_progress(progress, cases=True)
@@ -122,6 +126,7 @@ def open_webpage(browser, url, case, version, package):
         iexplorer(browser, version, case, package, url)
     if browser == 'edge':
         edge(browser, version, case, package, url)
+
 
 if __name__ == '__main__':
     main()
